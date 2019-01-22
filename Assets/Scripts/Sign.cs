74aa6e0361c749418b971f091024ec4236674b44
Ryan F <bammeh@gmail.com>
2019-01-21 23:52:58 -0500

    Video 10
    Added a sign. Interact with SPACE. when you leave collider, sign goes away.
    Added Dialog Box for sign.

---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange) // Are we pressing space and in range?
+        {
+            if (dialogBox.activeInHierarchy) // is the dialog box already active?
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog; // Set text in dialog box to text specified.
+            }
+        }
+    }
+
+    //When player enters the Sign's Collider area.
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox.SetActive(false); // if we leave teh collider, close the dialog box
+        }
+    }
+}
