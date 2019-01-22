2663e7eb80ebb5ea73d92ba738c5325e16eb6af5
Ryan F <bammeh@gmail.com>
2019-01-21 23:24:59 -0500

    End of Vid 9 - Room Place Text
    Added place text title cards like Dark Souls. When you enter a room, the room name pops up on the screen and fades.

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
 {
     public Vector2 cameraChange; // how much to shift camera
     public Vector3 playerChange; // how much will i shift player
     private CameraMovement cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +32,19 @@ public class RoomMove : MonoBehaviour
             cam.minPosition += cameraChange;
             cam.maxPosition += cameraChange;
             other.transform.position += playerChange;
+            if (needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
         }
+
+    }
+
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true); // set place name object active
+        placeText.text = placeName; // Set text on object to place name
+        yield return new WaitForSeconds(4); // Wait 4 seconds
+        text.SetActive(false); //set place name object inactive.
     }
 }
