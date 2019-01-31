5d9ff7e06f89897fde2ce5d608c1d45621b6e723
Ryan F <bammeh@gmail.com>
2019-01-31 02:40:11 -0500

    Video 26
    Added more rooms.
    Added navigation to those rooms.
    This incluided fixing the camera to move properly with the room bounds.
    
    All rooms are 25x25.

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -27,7 +27,7 @@ public class RoomMove : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !other.isTrigger) // doesnt do double trigger
         {
             cam.minPosition += cameraChange;
             cam.maxPosition += cameraChange;
