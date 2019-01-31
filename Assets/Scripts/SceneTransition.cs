8dc5beffff10c71961ae13ddec251ac6197494ff
Ryan F <bammeh@gmail.com>
2019-01-31 03:48:58 -0500

    Video 29
    Added Fade panels between scenes.

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -8,13 +8,40 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition; 
     public VectorValue playerStorage; // position player should spawn at.
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait; //minimum loading time.
+
+    void Awake() // Gets called before start()
+    {
+        if (fadeInPanel != null) // Did we assign a fade for this scene?
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);//Kill it after 1 sec.
+        }
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !other.isTrigger)
         {
             playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCo());
+            //SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+    public IEnumerator FadeCo()
+    {
+        if (fadeOutPanel != null) // Did we assign a fade for this scene?
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad); //load scene while loading fade panel.
+        while (!asyncOperation.isDone)
+        {
+            yield return null;
         }
     }
 }
