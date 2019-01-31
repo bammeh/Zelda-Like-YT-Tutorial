11717c4e3cd23ecd399175cb1ff6f472ef054c87
Ryan F <bammeh@gmail.com>
2019-01-31 00:43:23 -0500

    Video 23
    Added SignalListener and Signal to prepare for Video 24.

---
+++ Assets/Scripts/SignalListener.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SignalListener : MonoBehaviour
+{
+    public Signal signal;
+    public UnityEvent signalEvent;
+
+    public void OnSignalRaised()
+    {
+        signalEvent.Invoke(); // calls event.
+    }
+
+    private void OnEnable()
+    {
+        signal.RegisterListener(this); // add to Listener
+    }
+
+    private void OnDisable()
+    {
+        signal.DeregisterListener(this); // Remove listener (saves memory).
+    }
+}
